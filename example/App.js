18d5d7f5a8ea1510b7f1339026476738838a2ba1
Christofer Olaison <christofer.olaison@isotop.se>
2017-09-06 08:04:24 +0200

    1.0.0-beta.0
    Signed-off-by: Christofer Olaison <christofer.olaison@isotop.se>

--- example/App.js
+++ example/App.js
@@ -1,15 +1,12 @@
 import React from 'react'
 
 const App = () => (
-  <div
-    style={{
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center',
-    }}
-  >
-    <p style={{ letterSpacing: 0.5, fontFamily: 'sans-serif', fontSize: 40 }}>
+  <div style={{ marginLeft: 20 }}>
+    <h1 style={{ letterSpacing: 0.5, fontFamily: 'sans-serif' }}>
       react-lib-boilerplate <span style={{ paddingLeft: 5 }} role='img' aria-label='avocado'>🥑</span>
+    </h1>
+    <p style={{ letterSpacing: 0.3, fontFamily: 'sans-serif' }}>
+      Boilerplate for creating React and JavaScript packages with ease.
     </p>
   </div>
 )
